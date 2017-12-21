50a66febabfd3e166241a7d01c0f14b6d5fa4b45
Matt Vinall <boyvinall@gmail.copm>
2017-12-21 09:58:45 +0000

    add dumb-init and consul-template

--- docker.mk
+++ docker.mk
@@ -70,9 +70,32 @@ define PROMPT
 endef
 endif
 
+#----------------------------------------------------------
+#  define a few helper rules for installing common apps
+
+CA_BUNDLE:=.cache/ca-bundle.crt
+DUMB_INIT:=.cache/dumb-init
+CONSUL_TEMPLATE:=.cache/consul-template
+
+CONSUL_TEMPLATE_VERSION?=0.19.4
+
 # Useful with a scratch container. Use in Dockerfile as:
 #
-#   ADD ca-bundle.crt /etc/pki/tls/certs/ca-bundle.crt
-ca-bundle.crt:
+#   ADD .cache/ca-bundle.crt /etc/pki/tls/certs/ca-bundle.crt
+$(CA_BUNDLE): .cache
 	curl -sL https://mkcert.org/generate/ > $@
 
+$(DUMB_INIT): .cache
+	$(call PROMPT,Downloading dumb-init)
+	curl -sL https://github.com/boyvinall/dumb-init/releases/download/latest/dumb-init > $@
+	chmod +x $@
+	touch $@
+
+$(CONSUL_TEMPLATE): .cache
+	$(call PROMPT,Downloading consul-template)
+	curl -sL https://releases.hashicorp.com/consul-template/$(CONSUL_TEMPLATE_VERSION)/consul-template_$(CONSUL_TEMPLATE_VERSION)_linux_amd64.tgz | tar -xz -f- -C$(dir $@)
+	chmod +x $@
+	touch $@
+
+.cache:
+	mkdir -p $@
